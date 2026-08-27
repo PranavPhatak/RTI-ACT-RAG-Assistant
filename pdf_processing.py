909b74f3aa8320e098fcf9e7e49ff1cdb3d2f03f
Pranav Phatak <pranavphatak99@gmail.com>
2026-08-27 20:28:43 +0530

    libraries imported

---
+++ pdf_processing.py
@@ -0,0 +1,6 @@
+import os
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_ollama import OllamaEmbeddings
+from langchain_community.vectorstores import FAISS
+
